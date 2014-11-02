db07066660aef90593b9b9c2c6945c32e66e9f19
Nader Morshed <morshed.nader@gmail.com>
2014-11-02 14:11:58 -0500

    Bug fixes

--- setup-my-schema.sh
+++ setup-my-schema.sh
@@ -2,8 +2,7 @@
 echo "******CREATING DOCKER DATABASE******"
 gosu postgres postgres --single <<- EOSQL
    CREATE DATABASE docker;
-   CREATE USER postgres;
-   ALTER USER postgres WITH PASSWORD '${POSTGRESQL_PASS}'
+   ALTER USER postgres WITH PASSWORD '${POSTGRESQL_PASS}';
    GRANT ALL PRIVILEGES ON DATABASE docker to postgres;
 EOSQL
 echo ""
